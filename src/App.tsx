0e6d413582f8c828467d87686681e12df8ad64b7
Justin Hung <justinhung1994@gmail.com>
2025-01-26 13:54:33 +1100

    removed default content and added name and title, added framer motion for bounce effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
+import { motion } from "framer-motion";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [show, setShow] = useState(true)
+  const [showH2, setShowH2] = useState(false)
+
+  const handleToggle = () => {
+    setShow(false);
+    setTimeout(() => {
+      setShow(true);
+    }, 100);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowH2(true);
+    }, 1000);
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {show && (<motion.div
+          initial={{ scale: 0 }}
+          animate={{
+            scale: [0, 1.1, 0.9, 1],
+            transition: { type: "spring", stiffness: 100, bounce: 1, damping: 10 },
+          }}
+          exit={{ scale: 0 }}
+        >
+          <h1 onClick={handleToggle}>
+            Justin Hung
+          </h1>
+        </motion.div>)
+        }
+        {!showH2 && (
+          <div style={{ height: '36px', marginTop: '0.83rem' }}></div>
+        )}
+        {showH2 && (
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{
+              scale: [0, 1.1, 0.9, 1],
+              transition: { type: "spring", stiffness: 100, bounce: 1, damping: 10 },
+            }}
+            exit={{ scale: 0 }}
+          >
+            <h2>Full Stack Developer</h2>
+          </motion.div>)
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
